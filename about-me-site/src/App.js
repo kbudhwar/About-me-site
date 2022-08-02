61364cf38ad7a7cc0a2a49392e266d9a63ac2519
kbudhwar <khushi.budhwar@gmail.com>
2022-08-02 19:51:23 -0400

    fixed some things

--- about-me-site/src/App.js
+++ about-me-site/src/App.js
@@ -10,10 +10,10 @@ function App() {
       </div>
       <div className="subText">
         I'm a second-year{" "}
-        <span style={{ fontWeight: "800" }}>Software-Engineering student</span> at<br></br>
+        <span style={{ fontWeight:"900" }}>Software-Engineering student</span> at<br></br>
         the University of Waterloo.
       </div>
-      <object className="faceIcon" type="image/svg+xml" data={FaceIcon} aria-label="face icon"></object>
+      {/* <object className="faceIcon" type="image/svg+xml" data={FaceIcon} aria-label="face icon"></object> */}
     </div>
   );
 }
